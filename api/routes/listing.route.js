a34d4d128ab2d0f207f5209e883c6d2ff70bdc6f
Abdul Musavveer Alji <abdulmusavveeralji@gmail.com>
2023-10-17 23:15:12 +0530

    create search api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -2,8 +2,8 @@ import express from "express";
 import {
   createListing,
   deleteListing,
-  getListing,
-  getUser,
+  getListingById,
+  getListings,
   updateListing,
 } from "../controllers/listing.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
@@ -13,7 +13,7 @@ const router = express.Router();
 router.post("/create", verifyToken, createListing);
 router.delete("/delete/:id", verifyToken, deleteListing);
 router.post("/update/:id", verifyToken, updateListing);
-router.get("/getListing/:id", getListing);
-router.get("/:id", verifyToken, getUser);
+router.get("/getListing/:id", getListingById);
+router.get("/get", verifyToken, getListings);
 
 export default router;
