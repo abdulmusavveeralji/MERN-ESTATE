8fcd29a925d24192d84a572cc1ea649c27904745
Abdul Musavveer Alji <abdulmusavveeralji@gmail.com>
2023-10-19 01:24:35 +0530

    Complete about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -1,7 +1,26 @@
-import React from 'react'
+import React from "react";
 
 export default function About() {
   return (
-    <div>about</div>
-  )
+    <div className="py-20 px-4 max-w-6xl mx-auto">
+      <h1 className="text-3xl font-bold mb-4 text-slate-700">
+        About MERN Estate
+      </h1>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Amet
+        doloremque tempora atque nesciunt quasi reiciendis? Inventore fugit,
+        non, quibusdam nobis et placeat aliquid obcaecati reiciendis optio
+        blanditiis odit quaerat rerum.
+      </p>
+      <p className="mb-4 text-slate-700">
+        Lorem ipsum, dolor sit amet consectetur adipisicing elit. Similique
+        voluptate, recusandae temporibus tempore repudiandae aliquid aliquam ex
+        doloremque animi iusto maiores cupiditate expedita. Quasi ad officia
+        sint molestias voluptatum earum! Lorem ipsum, dolor sit amet consectetur
+        adipisicing elit. Autem ut dignissimos ab ipsa cum obcaecati in ducimus
+        quis est? Error voluptatem temporibus fuga officia asperiores! Sequi
+        enim praesentium assumenda harum.
+      </p>
+    </div>
+  );
 }
