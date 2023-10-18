b034417f7a29f095c3794109c2b36d5c8f2311fd
Abdul Musavveer Alji <abdulmusavveeralji@gmail.com>
2023-10-19 01:40:01 +0530

    import fix

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
-import SignIn from "./pages/signin";
+import SignIn from "./pages/signIn";
 import SignUp from "./pages/SignUp";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
