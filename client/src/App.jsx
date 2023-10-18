9de781c6863737102148b4b06a0d566e82dd73ae
Abdul Musavveer Alji <abdulmusavveeralji@gmail.com>
2023-10-19 01:43:29 +0530

    import issue

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
