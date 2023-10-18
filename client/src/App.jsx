18e97ba481cb0d12f0b08cb6a3555d85a6294729
Abdul Musavveer Alji <abdulmusavveeralji@gmail.com>
2023-10-19 01:31:50 +0530

    add pre installation

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -3,7 +3,6 @@ import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import SignIn from "./pages/signin";
 import SignUp from "./pages/SignUp";
-import About from "./pages/about";
 import Profile from "./pages/Profile";
 import Header from "./components/Header";
 import PrivateRoute from "./components/PrivateRoute";
@@ -11,6 +10,7 @@ import CreateListing from "./pages/CreateListing";
 import UpdateListing from "./pages/UpdateListing";
 import Listing from "./pages/Listing";
 import Search from "./pages/search";
+import About from "./pages/About";
 
 export default function App() {
   return (
